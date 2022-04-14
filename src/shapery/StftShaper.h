86cbb7da7576ab7bd6f1dcc1188c35f34e8dfc72
Josiah Chapman <josiah.chapman@gmail.com>
2022-04-13 22:40:43 -0700

    compilable for armmmmmmm

--- src/shapery/StftShaper.h
+++ src/shapery/StftShaper.h
@@ -1,8 +1,13 @@
 #ifndef ANALOGORIUM_STFTSHAPER_H
 #define ANALOGORIUM_STFTSHAPER_H
 
+#ifdef USE_MUFFT
 #include <fft.h>
 #include <fft_internal.h>
+#else
+#include <NE10.h>
+#endif // USE_MUFFT
+
 #include <complex>
 #include "Shaper.h"
 
@@ -20,9 +25,15 @@ private:
     int windowSize;
     int fftSize;
     int stftSize;
+#ifdef USE_MUFFT
     float *fftInput;
     cfloat *fftOutput;
     mufft_plan_1d *fftPlan;
+#else
+    ne10_float32_t *fftInput;
+    ne10_fft_cpx_float32_t *fftOutput;
+    ne10_fft_r2c_cfg_float32_t fftPlan;
+#endif // USE_MUFFT
     std::list<std::shared_ptr<Signal>> inputSignals;
 
     float hammingWindow(int sampleNumber) const;
